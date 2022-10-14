f3410a5e08a2e5c59b738b03bd016daa406ee708
amzdevsam <111421994+amzdevsam@users.noreply.github.com>
2022-10-14 15:57:07 +0200

    second commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet]
+        public async  Task<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users;
+            return await users.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUser(int id)
+        {
+            try
+            {
+                var user = _context.Users.FindAsync(id);
+                return await user;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
